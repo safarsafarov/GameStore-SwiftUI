f2a6576001ad9e99bf6dc26ddf65586a7840071a
safarsafarov <safarov.safar@hotmail.com>
2020-11-02 17:51:20 +0800

    commit ✨

---
+++ GameStore-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameStore-SwiftUI
+//
+//  Created by Safar Safarov on 2020/11/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameStore-SwiftUI/GameStore_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStore_SwiftUIApp.swift
+//  GameStore-SwiftUI
+//
+//  Created by Safar Safarov on 2020/11/2.
+//
+
+import SwiftUI
+
+@main
+struct GameStore_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
