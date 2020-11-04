f4ef2b4f2f3f3a424d3b3fa3a3e8a5dddd7eee39
safarsafarov <safarov.safar@hotmail.com>
2020-11-05 01:58:53 +0800

    commit ✨

---
+++ GameStore-SwiftUI/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  GameStore-SwiftUI
+//
+//  Created by Safar Safarov on 2020/11/5.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
